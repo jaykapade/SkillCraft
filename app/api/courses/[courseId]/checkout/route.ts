d7660222f484f5120ed2dbcc1a156009dfd12b44
Jay Kapade <jazzdestructor@gmail.com>
2023-10-23 12:53:17 +0530

    Integrated Stripe for checkout using webhook and checkout api and added webhook to public routes

---
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -0,0 +1,96 @@
+import Stripe from "stripe";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+
+type ParamsProps = {
+  params: {
+    courseId: string;
+  };
+};
+
+export async function POST(req: Request, { params }: ParamsProps) {
+  try {
+    const user = await currentUser();
+    if (!user || !user.id || !user.emailAddresses?.[0]?.emailAddress) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!course) return new NextResponse("Course not found", { status: 404 });
+
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId: user.id,
+          courseId: params.courseId,
+        },
+      },
+    });
+
+    if (purchase) return new NextResponse("Already purchased", { status: 400 });
+
+    const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: course.title,
+            description: course.description!,
+          },
+          unit_amount: Math.round(course.price! * 100),
+        },
+      },
+    ];
+
+    let stripeCustomer = await db.stripeCustomer.findUnique({
+      where: {
+        userId: user.id,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+
+    if (!stripeCustomer) {
+      const customer = await stripe.customers.create({
+        email: user.emailAddresses?.[0]?.emailAddress,
+      });
+
+      stripeCustomer = await db.stripeCustomer.create({
+        data: {
+          userId: user.id,
+          stripeCustomerId: customer.id,
+        },
+      });
+    }
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL!;
+    console.log("🚀 ~ baseUrl:", baseUrl);
+
+    const session = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.stripeCustomerId,
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${baseUrl}/courses/${course.id}?success=1`,
+      cancel_url: `${baseUrl}/courses/${course.id}?canceled=1`,
+      metadata: {
+        courseId: course.id,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json({ url: session.url });
+  } catch (error) {
+    console.error("[COURSE_ID_CHECKOUT]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
