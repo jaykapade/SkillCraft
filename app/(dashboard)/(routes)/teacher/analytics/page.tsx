8a2032ae6dc82776672f354c74e22f826928a192
Jay Kapade <jazzdestructor@gmail.com>
2023-10-24 12:33:18 +0530

    Added Teacher Analytics Page

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-type Props = {};
+import { getAnalytics } from "@/actions/getAnalytics";
+import DataCard from "./_components/DataCard";
+import Chart from "./_components/Chart";
 
-const AnalyticsPage = (props: Props) => {
-  return <div>AnalyticsPage</div>;
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 };
 
 export default AnalyticsPage;
