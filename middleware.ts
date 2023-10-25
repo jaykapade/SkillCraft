730a9c477992d9029aa99b78a55ade9e36050a1f
Jay Kapade <jazzdestructor@gmail.com>
2023-10-25 22:51:06 +0530

    Added Teacher Based middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { isTeacher } from "./lib/teacher";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhook"],
+  publicRoutes: ["/api/webhook", "/api/test"],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    const pathname = req.nextUrl.pathname;
+    if (
+      auth.isApiRoute &&
+      pathname.startsWith("/api/courses") &&
+      req.method !== "GET"
+    ) {
+      if (isTeacher(auth.userId)) {
+        console.log("Teacher Route");
+      } else return new NextResponse("Unauthorized", { status: 401 });
+    }
+  },
 });
 
 export const config = {
